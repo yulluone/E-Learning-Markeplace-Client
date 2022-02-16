759d896b5a49c53136cc80ef7c7845845af2d530
joshuamwo <38428314+joshuamwo@users.noreply.github.com>
2022-02-16 19:03:28 +0300

    commit

--- pages/login.js
+++ pages/login.js
@@ -1,8 +1,78 @@
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { SyncOutlined } from "@ant-design/icons";
+import Link from "next/link";
+
 const Login = () => {
+  const [email, setEmail] = useState("josh@gmail.com");
+  const [password, setPassword] = useState("yullujosh");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    //
+    setLoading(true);
+    const data = await axios
+      .post(`/auth/login`, {
+        email,
+        password,
+      })
+      .then(
+        (response) => {
+          var result = response.data;
+          console.log("LOGIN RESPONSE", result);
+
+          // setLoading(false);
+        },
+        (error) => {
+          // console.log(error);
+          toast.error(error.response.data);
+          setLoading(false);
+        }
+      );
+    // console.log(data);
+  };
+
   return (
     <>
       <h1 className="jumbotron text-center bg-primary square">Login</h1>
-      <p>login page</p>
+
+      <div className="container col-md-4 offset-md-4 pb-5">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            className="form-control mb-4 p-4"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter email"
+            required
+          />
+
+          <input
+            type="password"
+            className="form-control mb-4 p-4"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter password"
+            required
+          />
+
+          <button
+            type="submit"
+            className="btn btn-block btn-primary"
+            disabled={!email || !password || loading}
+          >
+            {loading ? <SyncOutlined spin /> : "Login"}
+          </button>
+        </form>
+        <p className="text-center p-3">
+          Not yet registered?{" "}
+          <Link href="/register">
+            <a> Register</a>
+          </Link>
+        </p>
+      </div>
     </>
   );
 };
--- pages/register.js
+++ pages/register.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { SyncOutlined } from "@ant-design/icons";
+import Link from "next/link";
 
 const Register = () => {
   const [name, setName] = useState("josh");
@@ -14,7 +15,7 @@ const Register = () => {
     //
     setLoading(true);
     const data = await axios
-      .post(`http://localhost:8000/auth/register`, {
+      .post(`/auth/register`, {
         name,
         email,
         password,
@@ -76,6 +77,12 @@ const Register = () => {
             {loading ? <SyncOutlined spin /> : "Submit"}
           </button>
         </form>
+        <p className="text-center p-3">
+          Already registered?{" "}
+          <Link href="/login">
+            <a> Login</a>
+          </Link>
+        </p>
       </div>
     </>
   );
