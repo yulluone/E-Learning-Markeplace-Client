d6dd42de3bb31dd319c0b88d8f8039b9cf3afb10
joshuamwo <joshuamuuo5@gmail.com>
2022-11-27 22:53:36 +0300

    lessons menu done

--- pages/user/course/[slug].js
+++ pages/user/course/[slug].js
@@ -2,6 +2,9 @@ import { useRouter } from "next/router";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import StudentRoute from "../../../components/routes/StudentRoute";
+import { Buton, Menu, Avatar } from "antd";
+
+const { Item } = Menu;
 
 const UserCourseView = () => {
   const router = useRouter();
@@ -10,6 +13,8 @@ const UserCourseView = () => {
   //states
   const [course, setCourse] = useState({ lessons: [] });
   const [loading, setLoading] = useState(false);
+  const [clicked, setClicked] = useState(-1);
+  const [collapsed, setCollapsed] = useState(false);
 
   //populate course && page
   useEffect(() => {
@@ -26,7 +31,31 @@ const UserCourseView = () => {
 
   return (
     <StudentRoute>
-      <pre>{JSON.stringify(course)}</pre>
+      {/* <pre>{JSON.stringify(course)}</pre> */}
+      <div className="row">
+        <div style={{ maxWidth: 320 }}>
+          <Menu
+            defaultSelectedKeys={[clicked]}
+            inlineCollapsed={collapsed}
+            style={{ height: "80vh", overflow: "hidden" }}
+          >
+            {course.lessons.map((lesson, index) => (
+              <Item
+                icon={<Avatar>{index + 1}</Avatar>}
+                key={index}
+                onClick={() => setClicked(index)}
+              >
+                {lesson.title.substring(0, 30)}
+              </Item>
+            ))}
+          </Menu>
+        </div>
+        <div className="col">
+          {clicked > -1
+            ? JSON.stringify(course.lessons[clicked])
+            : course.description}
+        </div>
+      </div>
     </StudentRoute>
   );
 };
