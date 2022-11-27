27211f104133df3251747971919b774534d7d8d7
joshuamwo <joshuamuuo5@gmail.com>
2022-11-27 23:51:24 +0300

    lesson videos and menu collapse done

--- pages/user/course/[slug].js
+++ pages/user/course/[slug].js
@@ -1,8 +1,15 @@
 import { useRouter } from "next/router";
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState, useEffect, createElement } from "react";
 import StudentRoute from "../../../components/routes/StudentRoute";
-import { Buton, Menu, Avatar } from "antd";
+import { Button, Menu, Avatar } from "antd";
+import ReactPlayer from "react-player";
+import ReactMarkdown from "react-markdown";
+import {
+  PlayCircleOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+} from "@ant-design/icons";
 
 const { Item } = Menu;
 
@@ -34,11 +41,22 @@ const UserCourseView = () => {
       {/* <pre>{JSON.stringify(course)}</pre> */}
       <div className="row">
         <div style={{ maxWidth: 320 }}>
+          <Button
+            className="text-primary mt-2 btn-block mb-2 "
+            onClick={() => setCollapsed(!collapsed)}
+          >
+            {createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined)}{" "}
+            {!collapsed && "Lessons"}
+          </Button>
           <Menu
             defaultSelectedKeys={[clicked]}
             inlineCollapsed={collapsed}
             style={{ height: "80vh", overflow: "hidden" }}
           >
+            <Item
+              icon={<Avatar>Zero</Avatar>}
+              onClick={() => setClicked(-1)}
+            >Course Description</Item>
             {course.lessons.map((lesson, index) => (
               <Item
                 icon={<Avatar>{index + 1}</Avatar>}
@@ -51,9 +69,39 @@ const UserCourseView = () => {
           </Menu>
         </div>
         <div className="col">
-          {clicked > -1
-            ? JSON.stringify(course.lessons[clicked])
-            : course.description}
+          {clicked > -1 ? (
+            <>
+              {course.lessons[clicked].video &&
+                course.lessons[clicked].video.Location && (
+                  <>
+                    <ReactPlayer
+                      className="player"
+                      controls
+                      url={course.lessons[clicked].video.Location}
+                      width="100%"
+                      height="80%"
+                    />
+                  </>
+                )}
+              <ReactMarkdown className="single-pos">
+                {course.lessons[clicked].content}
+              </ReactMarkdown>
+            </>
+          ) : (
+            <>
+              <ReactMarkdown>{course.description}</ReactMarkdown>
+              <div className="d-flex justify-content-center p-5">
+                <div className="text-center p-5">
+                  <PlayCircleOutlined
+                    className="text-primary display-1 p-5"
+                    onClick={() => {
+                      setClicked(1);
+                    }}
+                  />
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </StudentRoute>
