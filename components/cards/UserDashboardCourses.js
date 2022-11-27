e7b5536755e4397d374a3a05f7adadb06124cb1c
joshuamwo <joshuamuuo5@gmail.com>
2022-11-27 17:55:16 +0300

    design changes to display user courses on dashboard

--- components/cards/UserDashboardCourses.js
+++ components/cards/UserDashboardCourses.js
@@ -4,24 +4,42 @@ import { PlayCircleOutlined } from "@ant-design/icons";
 
 const UserDashboardCourses = ({ courses, user }) => {
   return (
-    <>
-      {/* <pre>{JSON.stringify(courses, null, 4)}</pre> */}
+    <div>
       {courses.map((course) => (
-        <div key={course._id} className="media pt-2 pb-1 row">
+        <div key={course._id} className="media pt-2 pb-1">
           <Avatar
             size={100}
             shape="square"
             src={`https://ipfs.filebase.io/ipfs/${course.image.cid}`}
           />
-          <Link href={`/user/course/${course.slug}`} className="ml-3">
-            <div className="col">
-              <h4>{course.name}</h4>
-              <PlayCircleOutlined className="h1 float-right" />
+
+          <div className=" pl-3 " style={{ width: "100%" }}>
+            <div className="row">
+              <div className="col">
+                <Link href={`/user/course/${course.slug}`} className="pointer ">
+                  <h5 className="mt-2 text-primary">{course.name}</h5>
+                </Link>
+                <p style={{ marginTop: "-10px" }}>
+                  {course.lessons.length} Lessons
+                </p>
+                <p
+                  className="text-muted"
+                  style={{ marginTop: "-15px", fontSize: "12px" }}
+                >
+                  By {course.instructor.name}
+                </p>
+              </div>
+
+              <div className="col-md-3 mt-3 text-center">
+                <Link href={`/user/course/${course.slug}`}>
+                  <PlayCircleOutlined className="h2 pointer text-primary" />
+                </Link>
+              </div>
             </div>
-          </Link>
+          </div>
         </div>
       ))}
-    </>
+    </div>
   );
 };
 
