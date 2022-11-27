07af050dec34da5bf6736303286efa1a6b78d58a
joshuamwo <joshuamuuo5@gmail.com>
2022-11-27 20:16:13 +0300

    fetch course for enrolled student done

--- pages/user/course/[slug].js
+++ pages/user/course/[slug].js
@@ -1,11 +1,29 @@
 import { useRouter } from "next/router";
-
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const UserCourseView = () => {
   const router = useRouter();
   const { slug } = router.query;
 
-  return <h1>{slug}</h1>;
+  //states
+  const [course, setCourse] = useState({ lessons: [] });
+  const [loading, setLoading] = useState(false);
+
+  //populate course && page
+  useEffect(() => {
+    if (!slug) return;
+    loadCourse();
+  }, [slug]);
+
+  const loadCourse = async () => {
+    const { data } = await axios.get(`/auth/course/${slug}`);
+    setCourse(data);
+  };
+
+  //handlers
+
+  return <pre>{JSON.stringify(course, null, 4)}</pre>;
 };
 
 export default UserCourseView;
